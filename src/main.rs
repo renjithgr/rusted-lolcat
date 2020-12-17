91c5f1b561b37be6a8471029cd9d8831ae5c6c02
Renjith <grenjithr@gmail.com>
2020-12-17 16:00:37 -0600

    Add the ability to specify a file name and print the contents

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,39 @@
 use std::io;
 use std::io::prelude::*;
+use std::fs::File;
+use std::io::BufReader;
 use std::f64::consts::PI;
 
 fn main() {
+
+    let filename = match std::env::args().nth(1) {
+        Some(n) =>  n,
+        None => String::from("")
+    };
+
+    if filename == "" {
+        process_standard_input();
+    } else if process_file(&filename).is_err() {
+        println!("Error opening file {}", filename);
+    }
+        
+}
+
+fn process_file(filename: &str)-> Result<(), io::Error>{
+    let f = File::open(filename)?;
+    let file = BufReader::new(&f);
+    for line in file.lines() {
+        for (i, c) in line.unwrap().char_indices() {
+            let (r, g, b) = rgb(0.1, i as f64);
+            print!("\x1b[38;2;{};{};{}m{}\x1b[0m", r, g, b, c);
+        }
+        println!();
+    }
+
+    Ok(())
+}
+
+fn process_standard_input() {
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         for (i, c) in line.unwrap().char_indices() {
