3ea873861eea14fb11ec8a5b89cc9cd97ca0ac4f
Renjith <grenjithr@gmail.com>
2020-12-16 22:05:03 -0600

    Beautiful rainbowgit add .

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
 use std::io;
 use std::io::prelude::*;
+use std::f64::consts::PI;
 
 fn main() {
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
-        for c in line.unwrap().chars() {
-            let (r, g, b) = rgb(c as i16);
+        for (i, c) in line.unwrap().char_indices() {
+            let (r, g, b) = rgb(0.1, i as f64);
             print!("\x1b[38;2;{};{};{}m{}\x1b[0m", r, g, b, c);
         }
         println!();
     }
 }
 
-fn rgb(c: i16) -> (u8, u8, u8) {
-    let freq = 0.1;
-    let i = c as f64;
-
-    let r = (freq * i + 0.0).sin() * 127.0 + 128.0;
-    let g = (freq * i + 2.0).sin() * 127.0 + 128.0;
-    let b = (freq * i + 4.0).sin() * 127.0 + 128.0;
+fn rgb(freq: f64, i: f64) -> (u8, u8, u8) {
+    let red = (freq * i + 0.0).sin() * 127.0 + 128.0;
+    let green = (freq * i + 2.0 * PI / 3.0).sin() * 127.0 + 128.0;
+    let blue = (freq * i + 4.0 * PI / 3.0).sin() * 127.0 + 128.0;
     
-    (r as u8, g as u8, b as u8)
+    (red as u8, green as u8, blue as u8)
 }
+
+
+// red   = Math.sin(freq*i + 0) * 127 + 128
+// green = Math.sin(freq*i + 2*Math::PI/3) * 127 + 128
+// blue  = Math.sin(freq*i + 4*Math::PI/3) * 127 + 128
