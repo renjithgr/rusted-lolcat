dd3bcaf171269c0f50a95ca88683b972d84d41ec
Renjith <grenjithr@gmail.com>
2020-12-18 17:22:05 -0600

    Moving arguments parsing to a separate module

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,19 @@ use std::io::BufReader;
 use std::f64::consts::PI;
 
 
+mod cli {
+    pub struct Params {
+        pub filenames: Vec<String>,
+        pub frequency: f64,
+        pub spread: f64
+    }
 
-fn main() {
-    let mut frequency = 0.1;    
-    let spread = 3.0;
-    let mut filenames = vec![];
+    pub fn process_params(input: Vec<String>) -> Params {
+        let mut frequency = 0.1;    
+        let spread = 3.0;
+        let mut filenames = vec![];
 
-    if std::env::args().len() == 1 {
-        process_standard_input();
-    } else {
-        let args: Vec<String> = std::env::args().collect();
-        let mut iter = args.iter();
+        let mut iter = input.iter();
         iter.next();
         while let Some(param) = iter.next() {
             if param == "-f" {
@@ -24,12 +26,28 @@ fn main() {
                     None => panic!("Invalid frequence value")
                 }
             } else {
-                filenames.push(param);
+                filenames.push(param.to_string());
             }
         }
+        return Params {
+            filenames: filenames,
+            frequency: frequency,
+            spread: spread
+        };
+    }
+}
 
-        for &filename in filenames.iter() {
-            if process_file(filename, frequency, spread).is_err() {
+
+fn main() {
+
+    if std::env::args().len() == 1 {
+        process_standard_input();
+    } else {
+        let args: Vec<String> = std::env::args().collect();
+        let params = cli::process_params(args);
+        
+        for filename in params.filenames.iter() {
+            if process_file(filename, params.frequency, params.spread).is_err() {
                 println!("Error opening file {}", filename);
             }
         }
