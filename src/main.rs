601b69997e9dee18389b0f25210c40c91a8dcd5c
Renjith <grenjithr@gmail.com>
2020-12-18 16:39:50 -0600

    Acceps frequency as parameter

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,12 @@ use std::fs::File;
 use std::io::BufReader;
 use std::f64::consts::PI;
 
+
+
 fn main() {
+    let mut frequency = 0.1;    
+    let spread = 3.0;
+    let mut filenames = vec![];
 
     if std::env::args().len() == 1 {
         process_standard_input();
@@ -12,20 +17,31 @@ fn main() {
         let args: Vec<String> = std::env::args().collect();
         let mut iter = args.iter();
         iter.next();
-        while let Some(filename) = iter.next() {
-            if process_file(filename).is_err() {
+        while let Some(param) = iter.next() {
+            if param == "-f" {
+                match iter.next() {
+                    Some(n) => frequency = n.parse::<f64>().unwrap(),
+                    None => panic!("Invalid frequence value")
+                }
+            } else {
+                filenames.push(param);
+            }
+        }
+
+        for &filename in filenames.iter() {
+            if process_file(filename, frequency, spread).is_err() {
                 println!("Error opening file {}", filename);
             }
         }
     }   
 }
 
-fn process_file(filename: &str)-> Result<(), io::Error>{
+fn process_file(filename: &str, frequency: f64, spread: f64)-> Result<(), io::Error>{
     let f = File::open(filename)?;
     let file = BufReader::new(&f);
     for line in file.lines() {
         for (i, c) in line.unwrap().char_indices() {
-            let (r, g, b) = rgb(0.1, i as f64);
+            let (r, g, b) = rgb(frequency, i as f64 / spread );
             print!("\x1b[38;2;{};{};{}m{}\x1b[0m", r, g, b, c);
         }
         println!();
