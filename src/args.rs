941b2c045fbc249e1f8396f86158c5e8dd891bb7
Renjith <grenjithr@gmail.com>
2020-12-21 15:10:10 -0600

    Add support for spread parameter

--- src/args.rs
+++ src/args.rs
@@ -6,16 +6,21 @@ pub struct Params {
 
 pub fn process_params(input: Vec<String>) -> Params {
     let mut frequency = 0.1;    
-    let spread = 3.0;
+    let mut spread = 3.0;
     let mut filenames = vec![];
 
     let mut iter = input.iter();
     iter.next();
     while let Some(param) = iter.next() {
-        if param == "-F" || param == "--freq" {
+        if param == "-f" || param == "--freq" {
             match iter.next() {
                 Some(n) => frequency = n.parse::<f64>().unwrap(),
                 None => panic!("Invalid frequency value")
+            }
+        } else if param == "-s" || param == "--spread" {
+            match iter.next() {
+                Some(n) => spread = n.parse::<f64>().unwrap(),
+                None => panic!("Invalid spread value")
             }
         } else {
             filenames.push(param.to_string());
@@ -49,7 +54,7 @@ mod tests {
 
     #[test]
     fn test_should_parse_frequency_parameter_short_param_name() {
-        let input: Vec<String> = vec_of_strings!["", "-F", "2.0", "filename-1.txt"];
+        let input: Vec<String> = vec_of_strings!["", "-f", "2.0", "filename-1.txt"];
         let params = process_params(input);
         assert_eq!(params.filenames.len(), 1);
         assert_eq!(params.frequency, 2.0);
@@ -61,6 +66,22 @@ mod tests {
         let params = process_params(input);
         assert_eq!(params.filenames.len(), 1);
         assert_eq!(params.frequency, 2.0);
+    }
+
+    #[test]
+    fn test_should_parse_spread_parameter_short_param_name() {
+        let input: Vec<String> = vec_of_strings!["", "-s", "2.0", "filename-1.txt"];
+        let params = process_params(input);
+        assert_eq!(params.filenames.len(), 1);
+        assert_eq!(params.spread, 2.0);
+    }
+
+    #[test]
+    fn test_should_parse_spread_parameter_long_param_name() {
+        let input: Vec<String> = vec_of_strings!["", "--spread", "2.0", "filename-1.txt"];
+        let params = process_params(input);
+        assert_eq!(params.filenames.len(), 1);
+        assert_eq!(params.spread, 2.0);
     }
 }
 
