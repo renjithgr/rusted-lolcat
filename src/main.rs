83b33c06002a14e58ba10867b445de69242174b2
Renjith <grenjithr@gmail.com>
2020-12-22 15:00:06 -0600

    Move all the calculations to the rgb function

--- src/main.rs
+++ src/main.rs
@@ -54,16 +54,17 @@ fn process_standard_input(frequency: f64, spread: f64) {
 
 fn rainbow_println(line: &str, frequency: f64, spread: f64) {
     for (i, c) in line.char_indices() {
-        let (r, g, b) = rgb(frequency, i as f64 / spread );
+        let (r, g, b) = rgb(frequency, spread, i as f64);
         print!("\x1b[38;2;{};{};{}m{}\x1b[0m", r, g, b, c);
     }
     println!();
 }
 
-fn rgb(freq: f64, i: f64) -> (u8, u8, u8) {
-    let red = (freq * i + 0.0).sin() * 127.0 + 128.0;
-    let green = (freq * i + 2.0 * PI / 3.0).sin() * 127.0 + 128.0;
-    let blue = (freq * i + 4.0 * PI / 3.0).sin() * 127.0 + 128.0;
+fn rgb(freq: f64, spread: f64, i: f64) -> (u8, u8, u8) {
+    let j = i / spread;
+    let red = (freq * j + 0.0).sin() * 127.0 + 128.0;
+    let green = (freq * j + 2.0 * PI / 3.0).sin() * 127.0 + 128.0;
+    let blue = (freq * j + 4.0 * PI / 3.0).sin() * 127.0 + 128.0;
     
     (red as u8, green as u8, blue as u8)
 }
